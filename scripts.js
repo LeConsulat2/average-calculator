a21c20a8ffc57611ae89cc3e6130db367d9c834a
LeConsulat2 <wooine2@gmail.com>
2024-08-07 18:50:57 +1200

    Updated without one-offs

--- scripts.js
+++ scripts.js
@@ -3,17 +3,22 @@ function calculateIncome() {
   const fortnightly =
     parseFloat(document.getElementById('fortnightly').value) || 0;
   const monthly = parseFloat(document.getElementById('monthly').value) || 0;
-  const oneoff = parseFloat(document.getElementById('oneoff').value) || 0;
 
   const weeklyIncome = weekly;
   const fortnightlyIncome = fortnightly / 2;
-  const monthlyIncome = monthly / 4.33;
-  const oneoffIncome = oneoff / 52;
+  const monthlyIncome = monthly / 4;
 
-  const totalWeeklyIncome =
-    weeklyIncome + fortnightlyIncome + monthlyIncome + oneoffIncome;
+  const totalWeeklyIncome = weeklyIncome + fortnightlyIncome + monthlyIncome;
 
-  document.getElementById(
-    'result',
-  ).innerText = `Total Weekly Income: $${totalWeeklyIncome.toFixed(2)}`;
+  const resultElement = document.getElementById('result');
+  resultElement.classList.remove('fade-in');
+  resultElement.classList.add('fade-out');
+
+  setTimeout(() => {
+    resultElement.innerText = `Total Weekly Income: $${totalWeeklyIncome.toFixed(
+      2,
+    )}`;
+    resultElement.classList.remove('fade-out');
+    resultElement.classList.add('fade-in');
+  }, 300);
 }
